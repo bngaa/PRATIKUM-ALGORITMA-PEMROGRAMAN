e12591040a750220ef7d2ab767ff4d7e6d1975c8
bunga aprillia <145094601+bngaa@users.noreply.github.com>
2023-12-04 14:09:48 +0700

    Add files via upload

---
+++ Jobsheet 10 - Enumerasi  Structure/Codingan/Percobaan codingan 1 jobsheet 10.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+enum hari {SENIN, SELASA, RABU, KAMIS, JUMAT, SABTU, MINGGU};
+
+int main(){
+	enum hari sekarang;
+	sekarang = RABU;
+	printf("Sekarang hari ke-%d", sekarang +1);
+	
+}
+#include <stdio.h>
+
