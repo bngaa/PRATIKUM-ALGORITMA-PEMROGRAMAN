10997aea3b9cd22dba50d566a347395f6ed0d886
bunga aprillia <145094601+bngaa@users.noreply.github.com>
2023-12-04 14:07:33 +0700

    Add files via upload

---
+++ Jobsheet 8 - Fungsi  Prosedur/Codingan/Percobaan codingan 6 jobsheet 8.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void kali_dua(int *num){
+    *num = *num * 2;
+}
+
+int main(){
+    int angka = 9;
+
+    // memanggil fungsi
+    kali_dua(&angka);
+
+    // mencetak isi variabel
+    // setelah fungsi dipanggil
+    printf("isi variabel angka = %d\n", angka);
+}
