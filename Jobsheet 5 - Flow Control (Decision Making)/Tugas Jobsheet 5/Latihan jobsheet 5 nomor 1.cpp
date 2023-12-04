783d4be509e2df6daa1873344e3cf04e366f69f0
bunga aprillia <145094601+bngaa@users.noreply.github.com>
2023-12-04 14:05:43 +0700

    Add files via upload

---
+++ Jobsheet 5 - Flow Control (Decision Making)/Tugas Jobsheet 5/Latihan jobsheet 5 nomor 1.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+	char op;
+    double angka1, angka2;
+
+    printf("Masukkan operator (+, -, *, /): ");
+    scanf("%c", &op);
+
+    printf("Masukkan dua angka: ");
+    scanf("%lf %lf", &angka1, &angka2);
+
+    switch (op) {
+    case '+':
+    printf("Hasil: %.2lf\n", angka1 + angka2);
+    break;
+
+	case '-':
+	printf("Hasil: %.2lf\n", angka1 - angka2);
+	break;
+
+	case '*':
+	printf("Hasil: %.2lf\n", angka1 * angka2);
+	break;
+
+	case '/':
+	if (angka2 != 0)
+	printf("Hasil: %.2lf\n", angka1 / angka2);
+	else
+	printf("Tidak bisa melakukan pembagian oleh 0.\n");
+	break;
+
+	default:
+	printf("Operator tidak valid.\n");
+    }
+
+    return 0;
+}
